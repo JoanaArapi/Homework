e37ecf76f3901c4dfcba636ba2f24674744f7aff
JoanaArapi <joana-arapi@hotmail.com>
2024-04-05 22:34:20 +0200

    New function files
    Mean and median function and test file

---
+++ functions.py
@@ -0,0 +1,22 @@
+def mean(data):
+    """
+    This function calculates the mean of a list of numerical data.
+    Parameters:
+    data is a list of numbers.
+    returns is the mean of the numbers.
+    """
+    return sum(data) / len(data)
+
+def median(data):
+    """
+    This function calculates the median of a list of numerical data.
+    Parameters:
+    data is a list of numbers.
+    returns is the median of the numbers.
+    """
+    data_sorted = sorted(data)
+    n = len(data_sorted)
+    if n % 2 == 0:
+        return (data_sorted[n // 2 - 1] + data_sorted[n // 2]) / 2
+    else:
+        return data_sorted[n // 2]
---
+++ test_functions.py
@@ -0,0 +1,7 @@
+from functions import mean, median
+
+def test_mean():
+    assert mean([2,3,4,5])==3.5
+
+def test_median():
+    assert median([2,3,4,5,6])==4
